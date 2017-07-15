fea9ea4b653eab07c68e399fb899792dd9189068
durbin-164 <masud.cseian@gmail.com>
2017-07-16 03:03:20 +0600

    Add files via upload

---
+++ 1124-Celebrityjeopardy.py
@@ -0,0 +1,16 @@
+'''
+Created on Jul 16, 2017
+
+@author: rasel
+'''
+# Uva 1124 - Celebrity jeopardy
+while True: 
+    try:
+        st = input()
+    except:
+        break
+    
+    if st=="":
+        break
+    
+    print(st)
